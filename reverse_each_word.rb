42577dabeaa45b0c545ef63cbf44b53275f553f1
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-15 01:00:30 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,13 @@ end
 # split.reverse_each { |letter| puts letter," "}
 # outputs sentence in reverse order; but not each word in reverse order
 #each_letter.each { |x| x.reverse.join(' ') }
+
+# def reverse_each_word(string)
+#   string.split.collect { |x| x.reverse}.join(' ')
+# end
+
+# def reverse_each_word(string)
+#   string.reverse.split.reverse.join(' ')
+# end
+
+# binding.pry
